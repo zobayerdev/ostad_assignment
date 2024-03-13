818babfcd534b808424f935c9cc5facf62b16912
zobayerdev <zobayer.dev@gmail.com>
2024-03-13 19:00:59 +0600

    complete code

---
+++ lib/assignment_dart_nayem.dart
@@ -0,0 +1,9 @@
+import 'package:assignment_dart_nayem/media.dart';
+
+void main() {
+  var media = Media();
+  var song = Song("Atif Islam");
+
+  media.play();
+  song.play();
+}
---
+++ lib/media.dart
@@ -0,0 +1,16 @@
+class Media {
+  void play() {
+    print("Playing media");
+  }
+}
+
+class Song extends Media {
+  String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print("Playing song by $artist");
+  }
+}
